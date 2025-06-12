c5f826ac116abc3b20f36c5c1053abe849b45d5c
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-06-12 16:25:41 +0200

    Add files via upload
    these are basic exercises on python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+quotient = number1 / number2
+print("addition of " + str(number1) + " and " + str(number2) + " is " + str(sum))
+print("subtraction of " + str(number1) + " and " + str(number2) + " is " + str(difference))
+print("multiplication of " + str(number1) + " and " + str(number2) + " is " + str(product))
+print("division of " + str(number1) + " and " + str(number2) + " is " + str(quotient))
