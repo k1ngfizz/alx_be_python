54ffaa06a5d9685fff0e1894f39f1c44f4cca36d
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-06-16 12:41:12 +0200

    Update test_simple_calculator.py
    i change some test errors

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,37 +1,30 @@
-# test_simple_calculator.py
-
 import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        """Set up a calculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
-        self.assertEqual(self.calc.add(-5, -3), -8)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
-        self.assertEqual(self.calc.subtract(5, 3), 2)
-        self.assertEqual(self.calc.subtract(0, 3), -3)
-        self.assertEqual(self.calc.subtract(-2, -4), 2)
-        self.assertEqual(self.calc.subtract(10, 10), 0)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 2), 3)
+        self.assertEqual(self.calc.subtract(2, 5), -3)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
 
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(4, 3), 12)
-        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(0, 5), 0)
-        self.assertEqual(self.calc.multiply(-3, -3), 9)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(-6, 3), -2)
-        self.assertEqual(self.calc.divide(0, 10), 0)
-        self.assertIsNone(self.calc.divide(5, 0))  # Division by zero
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertIsNone(self.calc.divide(5, 0))  # Division by zero test
 
 if __name__ == '__main__':
     unittest.main()
