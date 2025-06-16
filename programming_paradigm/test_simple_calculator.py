b542893ea4f8cc31aaae061ab34c095e3c92c042
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-06-16 12:44:20 +0200

    Update test_simple_calculator.py
    changed from dived to division

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -21,7 +21,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(0, 5), 0)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(9, 3), 3)
         self.assertIsNone(self.calc.divide(5, 0))  # Division by zero test
