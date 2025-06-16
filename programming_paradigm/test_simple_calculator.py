2746c1e76bbf3642b932734639c1846d381d9187
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-06-16 12:29:30 +0200

    Add files via upload

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,37 @@
+# test_simple_calculator.py
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up a calculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-5, -3), -8)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 3), -3)
+        self.assertEqual(self.calc.subtract(-2, -4), 2)
+        self.assertEqual(self.calc.subtract(10, 10), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
+        self.assertEqual(self.calc.multiply(-3, -3), 9)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+        self.assertEqual(self.calc.divide(0, 10), 0)
+        self.assertIsNone(self.calc.divide(5, 0))  # Division by zero
+
+if __name__ == '__main__':
+    unittest.main()
